ca2a2bc5487c84c5fafe4cfc122b2ca3a1857c01
Ryan Whitley <apollolm@gmail.com>
2014-07-08 21:12:56 -0700

    Working on filling out pages. Added Mustache for nav tempting. Filling out team page with images.

---
+++ js/right-menu.js
@@ -0,0 +1,14 @@
+
+{
+  "nav"
+:
+  [
+    { "url": "program.html", "title": "About the Program" },
+    { "url": "become-a-student.html", "title": "Become a Student" },
+    { "url": "companies.html", "title": "For Companies" },
+    { "url": "volunteer.html", "title": "Volunteer" },
+    { "url": "faq.html", "title": "FAQ" },
+    { "url": "team.html", "title": "Our Team" },
+    { "url": "contact.html", "title": "Contact Us" }
+  ]
+}
