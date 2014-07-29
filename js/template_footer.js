56bd6aea78eff0db4ac0836d2f556dc9723075d5
Ryan Whitley <apollolm@gmail.com>
2014-07-28 21:48:32 -0700

    New footer links. Changing language on home page.

--- js/template_footer.js
+++ js/template_footer.js
@@ -5,7 +5,7 @@
     { "url": "http://www.meetup.com/Tech-Diversified", "iconName": "icon-meetup", "title": "Meetup" },
     { "url": "https://twitter.com/TechDiversified", "iconName": "fa-twitter", "title": "Twitter" },
     { "url": "https://facebook.com/TechDiversified", "iconName": "fa-facebook", "title": "Facebook" },
-    { "url": "https://linkedin.com/TechDiversified", "iconName": "fa-linkedin", "title": "Linkedin" }
+    { "url": "https://www.linkedin.com/company/techdiversified", "iconName": "fa-linkedin", "title": "Linkedin" }
 
 
   ]
