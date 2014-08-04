d87dc146498beb30ff77c7eac3c0882da2c74ecb
Ryan Whitley <apollolm@gmail.com>
2014-08-03 22:49:57 -0700

    Updating GetInvolved. Added menu item for events.

--- js/right-menu.js
+++ js/right-menu.js
@@ -4,8 +4,9 @@
 :
   [
     { "url": "program.html", "title": "About the Program" },
-    { "url": "signup.html", "title": "Become a Student" },
-    { "url": "getinvolved.html", "title": "Get Involved" },
+    { "url": "events.html", "title": "Upcoming Events" },
+    { "url": "signup.html", "title": "Join Us" },
+    { "url": "getinvolved.html", "title": "Help Us" },
     { "url": "team.html", "title": "Our Team" },
     { "url": "faq.html", "title": "FAQ" }
   ]
