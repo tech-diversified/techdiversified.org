881b6210d62e4678572e538925e98caa63bdf09f
Ryan Whitley <apollolm@gmail.com>
2014-08-03 21:57:39 -0700

    Working on Signup or JoinUs page.

--- js/right-menu.js
+++ js/right-menu.js
@@ -4,7 +4,7 @@
 :
   [
     { "url": "program.html", "title": "About the Program" },
-    { "url": "become-a-student.html", "title": "Become a Student" },
+    { "url": "signup.html", "title": "Become a Student" },
     { "url": "getinvolved.html", "title": "Get Involved" },
     { "url": "team.html", "title": "Our Team" },
     { "url": "faq.html", "title": "FAQ" }
