e4e7dc4c43b67a997798d7dd18fb18e3b32e2335
Ryan Whitley <apollolm@gmail.com>
2014-08-21 22:06:38 -0700

    /Users/ryanwhitley/Documents/Github/techdiversified.org/js/right-menu.js

--- js/right-menu.js
+++ js/right-menu.js
@@ -5,8 +5,7 @@
   [
     { "url": "program.html", "title": "About the Program" },
     { "url": "events.html", "title": "Upcoming Events" },
-    { "url": "signup.html", "title": "Join Us" },
-    { "url": "getinvolved.html", "title": "Help Us" },
+    { "url": "getinvolved.html", "title": "Get Involved" },
     { "url": "team.html", "title": "Our Team" },
     { "url": "faq.html", "title": "FAQ" },
     { "url": "contact.html", "title": "Contact Us" }
