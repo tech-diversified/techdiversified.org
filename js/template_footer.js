98980f5cca17fd969fbb1034e8da680c2dc2c381
Ryan Whitley <apollolm@gmail.com>
2014-08-23 19:54:26 -0700

    working on brickwall

--- js/template_footer.js
+++ js/template_footer.js
@@ -6,7 +6,5 @@
     { "url": "https://twitter.com/TechDiversified", "iconName": "fa-twitter", "title": "Twitter" },
     { "url": "https://facebook.com/TechDiversified", "iconName": "fa-facebook", "title": "Facebook" },
     { "url": "https://www.linkedin.com/company/techdiversified", "iconName": "fa-linkedin", "title": "Linkedin" }
-
-
   ]
 }
